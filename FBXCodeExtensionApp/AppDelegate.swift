99d8a661999d1cae74e3d6b1074be1923019f7a0
Xiang Li <lixiang@baixing.com>
2016-11-04 20:10:27 +0800

    Initial Commit

---
+++ FBXCodeExtensionApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FBXCodeExtensionApp
+//
+//  Created by Xiang Li on 04/11/2016.
+//  Copyright © 2016 Xiang Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
